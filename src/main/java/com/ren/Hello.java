ec5507557c4afbb27f467f363ec1faf57fa6342a
ron208888 <ron208888@gmail.com>
2020-07-05 18:22:10 +0800

    o

---
+++ src/main/java/com/ren/Hello.java
@@ -0,0 +1,10 @@
+package com.ren;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+        Person p = new Person();
+        p.hello();
+        
+    }
+}
